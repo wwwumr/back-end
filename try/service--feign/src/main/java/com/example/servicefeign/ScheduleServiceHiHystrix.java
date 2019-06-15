042b8144bd27e3403b3f9da7b370a099271b00bf
wwwumr <1127858301@qq.com>
2019-06-15 11:43:00 +0800

    try new cloud

---
+++ try/service--feign/src/main/java/com/example/servicefeign/ScheduleServiceHiHystrix.java
@@ -0,0 +1,12 @@
+package com.example.servicefeign;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduleServiceHiHystrix implements ScheduleServiceHi {
+    
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
+}
